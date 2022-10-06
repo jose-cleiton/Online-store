1362c8d8e1641601dc61591eefb175b50816a720
jose-cleiton <cleitons835>
2022-10-06 10:34:17 -0300

    fix: Importações

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 
 import { ThemeProvider } from "@emotion/react";
 import { BrowserRouter } from "react-router-dom";
-import Header from "./Organisms/Header";
 import { AppRoutes } from "./routes";
 import { LightTheme } from './shared/themes/Light';
 import Frontend from "./templates/Frontend";
