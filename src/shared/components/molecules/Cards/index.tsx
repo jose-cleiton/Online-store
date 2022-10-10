fd0231f56a45b263b7cb2c7088cb496a9ae0de62
jose-cleiton <cleitons835>
2022-10-10 16:06:42 -0300

    feat : colocando Hover

--- src/shared/components/molecules/Cards/index.tsx
+++ src/shared/components/molecules/Cards/index.tsx
@@ -4,7 +4,7 @@ import { IQueriedRepositories } from '../../../../interfaces/interfaces';
 
 import Products from '../../atomos/img/product';
 import * as S from './styles';
-const query = 'brinquedos';
+const query = 'celular';
   
 const URL =`https://api.mercadolibre.com/sites/MLB/search?q=${query}`;
 
--- src/shared/components/molecules/Cards/styles.ts
+++ src/shared/components/molecules/Cards/styles.ts
@@ -10,6 +10,8 @@ width:55vw;
 height: 100vw;
 padding: 20px;
 
+
+
 img 
 {
 
@@ -26,7 +28,32 @@ ul
   align-items: center;
   list-style: none;
 
+  li{
+    position: relative;
+    :after 
+    {
+      content: "Cleiton";
+      position: absolute;
+        top: 0;
+    }
+
+
+:active{
+  transform: translate(-1px, 1px);
 }
+:hover{
+  box-shadow: 0px 16px 20px  rgba(0,0,0,0.3);
+  transition: 0.2s ease-in-out;
+  :after{
+    content: "clique para comprar!";
+
+  }
+}
+  }
+
+
+}
+
 
 
 
