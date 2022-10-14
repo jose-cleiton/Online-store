6dfbfd98975f55d908886cca82bcc6cb0e0abab3
jose-cleiton <cleitons835>
2022-10-13 21:27:30 -0300

    fix tipando

--- src/interfaces/interfaces.ts
+++ src/interfaces/interfaces.ts
@@ -271,23 +271,191 @@ export interface  ICategory {
     name: string;
 }
 
-export interface IProduct {
 
-
-    
+export interface Results {
     id: string;
+    site_id: string;
     title: string;
-    price: {
-        currency: string;
+    seller: {},
+    price: number;
+    prices: [{
+      id: string;
+      prices: [
+        {
+            id: string;
+            type: string;
+            amount: number;
+            regular_amount: null;
+            currency_id: string;
+            last_updated: string;
+            conditions: {
+                context_restrictions: string[];
+                start_time: null;
+                end_time: null;
+                eligible: boolean;
+            },      
+            exchange_rate_context: string;
+            metadata: { }
+        }
+      ];
+      presentation: {
+        display_currency: string;
+      };
+      payment_method_prices: [
+      ];
+      reference_prices: [
+      ];
+      purchase_discounts: [
+      ];
+    }];
+    sale_price: null,
+    currency_id: string;
+    available_quantity: number;
+    sold_quantity: number;
+    buying_mode: string;
+    listing_type_id: string;
+    stop_time: string;
+    condition: string;
+    permalink: string;
+    thumbnail: string;
+    thumbnail_id: string;
+    accepts_mercadopago: boolean;
+    installments: {
+        quantity: number;
         amount: number;
-        decimals: number;
+        rate: number;
+        currency_id: string;
+    };
+    
+    address: {
+        state_id: string;
+        state_name: string;
+        city_id: string;
+        city_name: string;
 
     };
-    picture: string;
-    condition: string;
-    free_shipping: boolean;
-    state_name: string;
+    promotions: [
+    ];
+    shipping: {
+        free_shipping: boolean;
+        mode: string;
+        tags: string[];
+        logistic_type: string;
+        store_pick_up: boolean;
 
+    };    
+    seller_address: {
+        id: string;
+        comment: string;
+        address_line: string;
+        zip_code: string;
+        country: {
+            id: string;
+            name: string;
+
+
+    };
+    state: {
+        id: string;
+        name: string;
+    };
+
+    city: {
+        id: string;
+        name: string;
+    };
+
+    latitude: string;
+    longitude: string;
+
+    attributes: [
+        {
+            id: string;
+            name: string;
+            value_id: string;
+            value_name: string;
+            value_struct: null;
+            values: [
+                {
+                    id: string;
+                    name: string;
+                    struct: null;
+                    source: number;
+
+                }
+
+            ];
+
+        }
+    ];
+
+    
+    differential_pricing: {
+      id: number;
+    };
+    original_price: null;
+    category_id: string;
+    official_store_id: null;
+    domain_id: string;
+    catalog_product_id: null;
+    tags: string[];
+    order_backend: number;
+    use_thumbnail_id: boolean;
+    offer_score: null;
+    offer_share: null;
+    match_score: null;
+    winner_item_id: null;
+    melicoin: null;
+    discounts: null;
+    inventory_id: null;
+  }
+}
+
+
+
+export interface IProduct {
+    site_id: string;
+    country_default_time_zone: string;
+    query: string;
+    paging: {
+        total: number;
+        offset: number;
+        primary_results: number;
+        limit: number;
+
+    };
+    results: Results[];
+    sort: {
+        id: string;
+        name: string;
+
+    };
+    available_sorts: [
+        {
+            id: string;
+            name: string;
+
+        }
+    ];
+    filters: [
+    ];
+    available_filters: [
+        {
+            id: string;
+            name: string;
+            type: string;
+            values: 
+            [
+                {
+                    id: string;
+                    name: string;
+                    results: number;
+
+                }
+
+            ];
+        }
+];
 }
 
 export type UserContextType = {   
@@ -298,24 +466,7 @@ export type UserContextType = {
     
 }
 
-const obj = {
-
-
-    
-    id:'',
-    title: '',
-    price: {
-        currency: '',
-        amount: 0,
-        decimals: 0,
-
-    },
-    picture: '',
-    condition: '',
-    free_shipping: false,
-    state_name: ''
-
-}
+const obj = {}
 
 
 
@@ -326,14 +477,7 @@ export const initialState = {
     setInfo: () => {},
   };
 
-//  export  type UserThemeContextType = {
-//     theme: boolean;
-//     setTheme: React.Dispatch<React.SetStateAction<boolean>>,
-//   }
-//   export const initialValue = {
-//     theme: true,
-//     setTheme: () => {}
-//   }
+
 
 
 
