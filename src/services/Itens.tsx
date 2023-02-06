8479ff473cf90b7c8a07a32f61c426e96f40f3b2
jose-cleiton <cleitons835@gmail.com>
2023-02-06 19:59:56 -0300

    feat: :sparkles: useProductQuery

--- src/services/Itens.tsx
+++ src/services/Itens.tsx
@@ -1,18 +1,9 @@
-import axios from 'axios';
-import { useQuery } from 'react-query';
-import { IQueriedRepositories } from '../interfaces/interfaces';
 import Products from '../shared/components/product';
-import { research } from '../interfaces/interfaces';
+import { useProductQuery } from './useProductQuery';
 
 const Itens = () => {
-      const URL =`https://api.mercadolibre.com/sites/MLB/search?q=${research()}`;
+  const data = useProductQuery();
 
-        const { data } = useQuery<IQueriedRepositories[]>('products', 
-        async () =>
-        {
-            const response = await axios.get(URL)          
-            return response.data.results
-        });  
       return data?.map(
         (
             item: any
